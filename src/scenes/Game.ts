4f6c0be11eb43be487244ec9e5cdaac69cb287e1
AllieS125 <143726636+AllieS125@users.noreply.github.com>
2024-11-07 07:17:48 -0800

    PlatformsAdded

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -5,6 +5,7 @@ export class Game extends Scene
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
     msg_text : Phaser.GameObjects.Text;
+    platforms: Phaser.Physics.Arcade.StaticGroup;
 
     constructor ()
     {
@@ -14,17 +15,16 @@ export class Game extends Scene
     create ()
     {
         this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
 
         this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
+        this.platforms = this.physics.add.staticGroup();
+
+        this.platforms.create(400, 568, 'platform').setScale(2).refreshBody();
+
+        this.platforms.create(600, 400, 'platform');
+        this.platforms.create(50, 250, 'platform');
+        this.platforms.create(750, 220, 'platform');
 
         this.input.once('pointerdown', () => {
 
