3ef3631fce4524434ab48e10842f0bf1693edc2b
Finley McMurtrie <FMcMurtrie25@holynames-sea.org>
2024-11-08 16:31:54 -0800

    keys work now

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -67,6 +67,6 @@ export class Preloader extends Scene
         });
 
         //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-        this.scene.start('MainMenu');//No commit
+        this.scene.start('MainMenu');
     }
 }
