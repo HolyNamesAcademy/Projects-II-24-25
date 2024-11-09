09f2a9c2a60eb101b16f2ed78c285288023ede5e
Alexander Bell-Towne <alexander@bell-towne.com>
2024-11-08 17:26:17 -0800

    Fix keyboard control

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -4,10 +4,10 @@ export class Game extends Scene
 {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+    msg_text: Phaser.GameObjects.Text;
     platforms: Phaser.Physics.Arcade.StaticGroup;
     player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
-    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
 
     constructor ()
     {
@@ -29,45 +29,36 @@ export class Game extends Scene
         this.platforms.create(750, 220, 'platform');
 
         this.player = this.physics.add.sprite(100, 450, 'addison');
-        
+
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
         this.player.setScale(5);
 
         this.physics.add.collider(this.player, this.platforms);
 
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('GameOver');
-
-        });
-        
+        this.cursors = this.input?.keyboard?.createCursorKeys();
     }
-    update () {
 
-    this.cursors = this.input.keyboard?.createCursorKeys();
-        if (this.cursors.left.isDown)
-            {
-                this.player.setVelocityX(-160);
-            
-                this.player.anims.play('left', true);
-            }
-            else if (this.cursors.right.isDown)
-            {
-                this.player.setVelocityX(160);
-            
-                this.player.anims.play('right', true);
-            }
-            else
-            {
-                this.player.setVelocityX(0);
-            
-                this.player.anims.play('turn');
-            }
-            
-            if (this.cursors.up.isDown && this.player.body.touching.down)
-            {
-                this.player.setVelocityY(-330);
-            }
+    update ()
+    {
+        if (this.cursors?.left.isDown) {
+            this.player.setVelocityX(-160);
+
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors?.right.isDown) {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play('right', true);
+        }
+        else {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play('turn');
+        }
+
+        if (this.cursors?.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330);
+        }
     }
 }
