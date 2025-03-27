b4a3a3d4697d241a9232f8746529589422846608
Finley McMurtrie <FMcMurtrie25@holynames-sea.org>
2025-03-27 07:29:18 -0700

    Start wall Flicker

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -8,10 +8,16 @@ export class Boot extends Scene {
     preload() {
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
-        this.load.image('titleBackground', 'assets/StartWall.png');
+        this.load.spritesheet('titleBackground', 'assets/Wall Start Screen.png', { frameWidth: 512, frameHeight: 384 });
     }
 
     create() {
+        this.anims.create({
+            key: 'titleBackground',
+            frames: this.anims.generateFrameNumbers('titleBackground', { frames: [0, 1, 2, 3] }),
+            frameRate: 1,
+            repeat: -1,
+        });
         this.scene.start('Preloader');
     }
 }
