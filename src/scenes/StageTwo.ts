1223e6cdbb050336db16b036b5e50f0300f328e5
AllieS125 <143726636+AllieS125@users.noreply.github.com>
2025-04-03 21:31:17 -0700

    no work

--- src/scenes/StageTwo.ts
+++ src/scenes/StageTwo.ts
@@ -6,7 +6,7 @@ import { Layout } from '../types';
 const layout: Layout = {
     objects: [
         { type: 'platform', x: 200, y: 200 },
-        { type: 'door', x: 200, y: 0, key: 'door2Key', next: { scene: 'MainLevel', coordinates: { x: 219, y: 468 }, scrollPosition: 30 } },
+        { type: 'door', x: 200, y: 0, key: 'door2Key', next: { scene: 'MainLevel', coordinates: { x: 219, y: 468 }, scrollPosition: 660 } },
     ],
 };
 
