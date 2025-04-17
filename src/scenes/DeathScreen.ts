b4964c5768071e501f9544698938debfd394fb28
Alexander Bell-Towne <alexander@bell-towne.com>
2025-04-17 13:09:43 -0700

    Fix deathscreen for addisons

--- src/scenes/DeathScreen.ts
+++ src/scenes/DeathScreen.ts
@@ -39,8 +39,15 @@ export class DeathScreen extends Scene {
         console.log('Deaths:', this.deaths);
         console.log('Character:', data.character);
 
-        // Push the new death to the array
-        this.deaths.push(data.character);
+        // In case they get to this screen without a character
+        // (e.g. if they die in the main menu) (this should not happen)
+        if (data.character) {
+            // Push the new death to the array
+            this.deaths.push(data.character);
+        }
+
+        // Filter out any null or undefined values from the deaths array
+        this.deaths = this.deaths.filter(death => death !== null && death !== undefined);
 
         // Store the updated deaths in localStorage
         localStorage.setItem('deaths', JSON.stringify(this.deaths));
@@ -95,7 +102,8 @@ export class DeathScreen extends Scene {
                 return;
             }
             let y = startY - countY * spacingY;
-            if (death === 'addison') {
+            if (death.toLowerCase().includes('addison')) {
+                console.log('Addison sprite detected', death);
                 y = y - 48; // Adjust for Addison's sprite being on the bottom;
             }
             const sprite = this.add.sprite(
